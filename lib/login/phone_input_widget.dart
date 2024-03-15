162cbb57886930fca9c86e6b1b01999339f8ebc8
Artem Luzin m <aratar006@gmail.com>
2024-03-15 20:24:33 +0700

    Поменять виджет для номера телефона

--- lib/login/phone_input_widget.dart
+++ lib/login/phone_input_widget.dart
@@ -1,8 +1,10 @@
 import 'dart:developer';
 import 'dart:io';
 
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 class PhoneInputWidget extends StatefulWidget {
   const PhoneInputWidget({super.key});
@@ -18,11 +20,17 @@ class _PhoneInputWidgetState extends State<PhoneInputWidget> {
 
   @override
   Widget build(BuildContext context) {
+    const blueColor = Color(0xFF99BFD4);
+    const errorBackgroundColor = Color(0xFFf9dee3);
+    const errorBorderColor = Color(0xFFE25C74);
+    var maskFormatter = MaskTextInputFormatter(
+        mask: '### ### ## ##',
+        filter: {"#": RegExp(r'[0-9]')},
+        type: MaskAutoCompletionType.lazy);
     return LayoutBuilder(builder: (context, constraints) {
       final height = MediaQuery.of(context).size.height;
       final width = MediaQuery.of(context).size.width;
-      // final maxHeight = constraints.maxHeight.toDouble();
-      // final maxWidth = constraints.hashCode.toDouble();
+
       return Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -31,8 +39,8 @@ class _PhoneInputWidgetState extends State<PhoneInputWidget> {
               if (_phoneError)
                 Material(
                   elevation: 10,
-                  color: const Color(0xFFE25C74),
-                  shadowColor: const Color(0xFFE25C74).withOpacity(0.2),
+                  color: errorBorderColor,
+                  shadowColor: errorBorderColor.withOpacity(0.2),
                   shape: const RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(30)),
                   ),
@@ -41,96 +49,101 @@ class _PhoneInputWidgetState extends State<PhoneInputWidget> {
                     width: double.infinity,
                   ),
                 ),
-              Positioned(
-                right: 0,
-                child: Container(
-                  height: 0.06 * height,
-                  width: width * 0.7,
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.only(
-                        topRight: Radius.circular(30),
-                        bottomRight: Radius.circular(30)),
-                    color: _phoneError ? const Color(0xFFf9dee3) : Colors.white,
-                  ),
-                ),
-              ),
-              Positioned(
-                left: 0,
-                child: Container(
-                  height: 0.06 * height,
-                  constraints: const BoxConstraints(
-                    maxWidth: 130,
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        bottomLeft: Radius.circular(30)),
-                    color: _phoneError
-                        ? const Color(0xFFf9dee3)
-                        : const Color(0xFFF8F8F8),
-                  ),
-                ),
-              ),
               Container(
                 height: 0.06 * height,
-                width: width,
+                width: double.infinity,
                 decoration: BoxDecoration(
+                  color: _phoneError ? errorBackgroundColor : Colors.white,
                   border: Border.all(
                     width: 0.5,
-                    color: !_phoneError
-                        ? const Color(0xFF99BFD4)
-                        : const Color(0xFFE25C74),
+                    color: !_phoneError ? blueColor : errorBorderColor,
                   ),
                   borderRadius: BorderRadius.circular(30),
                 ),
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.transparent),
-                    borderRadius: BorderRadius.circular(29),
-                  ),
-                  child: InternationalPhoneNumberInput(
-                    spaceBetweenSelectorAndTextField: 12,
-                    selectorConfig: const SelectorConfig(
-                      setSelectorButtonAsPrefixIcon: false,
-                      trailingSpace: false,
+                child: Row(children: [
+                  Container(
+                    height: double.maxFinite,
+                    constraints: const BoxConstraints(
+                      maxWidth: 90,
                     ),
-                    onInputChanged: (PhoneNumber phoneNumber) {
-                      log(phoneNumber.phoneNumber.toString());
-                      setState(() {
-                        if (phoneNumber.phoneNumber != null) {
-                          if (phoneNumber.phoneNumber!.length > 4 &&
-                              phoneNumber.phoneNumber!.length <= 5) {
-                            _phoneError = true;
-                          } else {
-                            _phoneError = false;
-                          }
-                        }
-                      });
-                    },
-                    initialValue: initialPhone,
-                    inputDecoration: const InputDecoration(
-                      border: InputBorder.none,
-                      hintStyle: TextStyle(
-                        color: Color(0xFFCECECE),
-                        fontFamily: 'SourceSansPro',
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          bottomLeft: Radius.circular(30)),
+                      color: _phoneError
+                          ? errorBackgroundColor
+                          : const Color(0xFFF8F8F8),
+                    ),
+                    child: Row(
+                      children: [
+                        SizedBox(width: width * 0.04),
+                        Image.asset('images/flag.png'),
+                        const Text(
+                          '  (+7) ▼',
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: 'SourceSansPro',
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(left: width * 0.03),
+                      decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.only(
+                            topRight: Radius.circular(30),
+                            bottomRight: Radius.circular(30)),
+                        color:
+                            _phoneError ? errorBackgroundColor : Colors.white,
                       ),
-                      hintText: '000 000 00 00',
+                      child: TextField(
+                        inputFormatters: [maskFormatter],
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: '000 000 00 00',
+                          hintStyle: TextStyle(
+                            color: Color(0xFFCECECE),
+                            fontFamily: 'SourceSansPro',
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                ]),
               ),
+
+              //       }
+              //     });
+              //   },
+              //   initialValue: initialPhone,
+              //   inputDecoration: const InputDecoration(
+              //     border: InputBorder.none,
+              //     hintStyle: TextStyle(
+              //       color: Color(0xFFCECECE),
+              //       fontFamily: 'SourceSansPro',
+              //     ),
+              //     hintText: '000 000 00 00',
+              //   ),
+              // ),
+              // ),
+              // ),
             ],
           ),
           if (_phoneError)
             Padding(
-              padding: EdgeInsets.only(left: width * 0.05),
-              child: const Text(
+              padding: EdgeInsets.only(left: width * 0.05, top: height * 0.005),
+              child: const AutoSizeText(
                 'пожалуйста, заполните это поле',
                 style: TextStyle(
                   color: Color(0xFFE25C74),
                   fontFamily: 'SourceSansPro',
-                  fontSize: 10,
+                  fontSize: 12,
                 ),
+                minFontSize: 8,
+                maxLines: 1,
               ),
             ),
         ],
