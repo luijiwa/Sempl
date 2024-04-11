c8a02c41394c7f78c8bc5d3b80a19eb0d6e6a04b
Artem Luzin m <aratar006@gmail.com>
2024-04-11 19:27:31 +0700

    Фиксы вёрстки экранов подтверждения

--- lib/src/feature/survey_order/ui/сonfirmation_order_type_one_screen.dart
+++ lib/src/feature/survey_order/ui/сonfirmation_order_type_one_screen.dart
@@ -16,6 +16,8 @@ class ConfirmationOrderTypeOneScreen extends StatelessWidget {
       body: LayoutBuilder(builder: (context, constraints) {
         final maxHeight = constraints.maxHeight.toDouble();
         final maxWidth = constraints.maxWidth.toDouble();
+        final bodyHeight = maxHeight * 0.58;
+
         logger.w(maxWidth * 0.078);
         return Stack(
           children: [
@@ -28,6 +30,7 @@ class ConfirmationOrderTypeOneScreen extends StatelessWidget {
                   decoration: const BoxDecoration(
                     image: DecorationImage(
                       fit: BoxFit.cover,
+                      alignment: Alignment.topCenter,
                       image: AssetImage("assets/images/face_header.png"),
                     ),
                   ),
@@ -47,96 +50,133 @@ class ConfirmationOrderTypeOneScreen extends StatelessWidget {
             ),
             Align(
               alignment: Alignment.bottomCenter,
-              child: Container(
-                width: double.maxFinite,
-                height: maxHeight * 0.6,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: AssetImage(
-                        "assets/images/survey_suggestion_background.png"),
-                  ),
-                  color: Color(0xFF99BFD4),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(40),
-                    topRight: Radius.circular(40),
-                  ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 22).copyWith(
-                    top: 0.03 * maxHeight,
-                  ),
-                  child: Column(children: [
-                    Padding(
-                      padding: EdgeInsets.only(right: maxWidth * 0.078),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          AutoSizeText(
-                            "Спасибо за ваши ответы!".toUpperCase(),
-                            style: const TextStyle(
-                                fontFamily: 'DrukCyr',
-                                fontSize: 50,
-                                color: Colors.white,
-                                height: 1),
-                            maxLines: 2,
-                          ),
-                          SizedBox(height: maxHeight * 0.0237),
-                          Padding(
-                            padding: EdgeInsets.only(right: maxWidth * 0.078),
-                            child: const AutoSizeText(
-                              'Товар будет отправлен в ближайшее время. Сроки и статус доставки можно отслеживать в личном кабинете',
-                              style: TextStyle(
-                                fontFamily: 'SourceSansPro',
-                                fontSize: 15,
-                                color: Colors.white,
-                              ),
-                              maxLines: 3,
-                            ),
-                          ),
-                        ],
+              child: Stack(
+                children: [
+                  Container(
+                    width: double.maxFinite,
+                    height: bodyHeight,
+                    decoration: const BoxDecoration(
+                      color: Color(0xFF99BFD4),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(40),
+                        topRight: Radius.circular(40),
                       ),
                     ),
-                    const Spacer(),
-                    Align(
-                      alignment: Alignment.bottomCenter,
-                      child: SizedBox(
-                        width: maxWidth,
-                        height: 0.055 * maxHeight,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            context.goNamed(
-                                AppRoutes.confirmationOrderTypeTwoScreen.name);
-                          },
-                          style: ElevatedButton.styleFrom(
-                            elevation: 0,
-                            shape: const StadiumBorder(),
-                            foregroundColor:
-                                const Color.fromARGB(255, 183, 222, 243),
-                            backgroundColor: Colors.white,
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
+                  ),
+                  Positioned(
+                    right: 0,
+                    bottom: 0,
+                    child: Image.asset(
+                      'assets/images/green_triangle.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Positioned(
+                    right: 0,
+                    bottom: maxHeight * 0.02,
+                    child: Image.asset(
+                      'assets/images/orange_star.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Positioned(
+                    left: 22,
+                    top: maxHeight * 0.28,
+                    child: Image.asset(
+                      'assets/images/yellow_star.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Container(
+                    width: double.maxFinite,
+                    height: bodyHeight,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(40),
+                        topRight: Radius.circular(40),
+                      ),
+                    ),
+                    child: Padding(
+                      padding:
+                          const EdgeInsets.symmetric(horizontal: 22).copyWith(
+                        top: 0.03 * maxHeight,
+                      ),
+                      child: Column(children: [
+                        Padding(
+                          padding: EdgeInsets.only(
+                              right: maxWidth * 0.078, top: maxHeight * 0.0237),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              AutoSizeText('НА ГЛАВНЫЙ ЭКРАН ',
+                              AutoSizeText(
+                                "Спасибо за ваши ответы!".toUpperCase(),
+                                style: const TextStyle(
+                                    fontFamily: 'DrukCyr',
+                                    fontSize: 50,
+                                    color: Colors.white,
+                                    height: 1),
+                                maxLines: 2,
+                              ),
+                              SizedBox(height: maxHeight * 0.0237),
+                              Padding(
+                                padding:
+                                    EdgeInsets.only(right: maxWidth * 0.078),
+                                child: const AutoSizeText(
+                                  'Товар будет отправлен в ближайшее время. Сроки и статус доставки можно отслеживать в личном кабинете',
                                   style: TextStyle(
                                     fontFamily: 'SourceSansPro',
-                                    fontSize: maxWidth > 320 ? 15 : 12,
-                                    color: const Color(0xFF86A6B8),
-                                  )),
-                              SvgPicture.asset(
-                                colorFilter: const ColorFilter.mode(
-                                    AppThemeColor.blueColor, BlendMode.srcIn),
-                                'assets/images/arrow_right.svg',
+                                    fontSize: 15,
+                                    color: Colors.white,
+                                  ),
+                                  maxLines: 3,
+                                ),
                               ),
                             ],
                           ),
                         ),
-                      ),
+                        const Spacer(),
+                        Align(
+                          alignment: Alignment.bottomCenter,
+                          child: SizedBox(
+                            width: maxWidth,
+                            height: 0.055 * maxHeight,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                context.goNamed(AppRoutes
+                                    .confirmationOrderTypeTwoScreen.name);
+                              },
+                              style: ElevatedButton.styleFrom(
+                                elevation: 0,
+                                shape: const StadiumBorder(),
+                                foregroundColor:
+                                    const Color.fromARGB(255, 183, 222, 243),
+                                backgroundColor: Colors.white,
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  AutoSizeText('НА ГЛАВНЫЙ ЭКРАН ',
+                                      style: TextStyle(
+                                        fontFamily: 'SourceSansPro',
+                                        fontSize: maxWidth > 320 ? 15 : 12,
+                                        color: const Color(0xFF86A6B8),
+                                      )),
+                                  SvgPicture.asset(
+                                    colorFilter: const ColorFilter.mode(
+                                        AppThemeColor.blueColor,
+                                        BlendMode.srcIn),
+                                    'assets/images/arrow_right.svg',
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        const BottomPadding(),
+                      ]),
                     ),
-                    const BottomPadding(),
-                  ]),
-                ),
+                  ),
+                ],
               ),
             )
           ],
--- lib/src/feature/survey_order/ui/сonfirmation_order_type_two_screen.dart
+++ lib/src/feature/survey_order/ui/сonfirmation_order_type_two_screen.dart
@@ -16,6 +16,8 @@ class ConfirmationOrderTypeTwoScreen extends StatelessWidget {
       body: LayoutBuilder(builder: (context, constraints) {
         final maxHeight = constraints.maxHeight.toDouble();
         final maxWidth = constraints.maxWidth.toDouble();
+        final bodyHeight = maxHeight * 0.58;
+
         logger.w(maxWidth * 0.078);
         return Stack(
           children: [
@@ -47,95 +49,124 @@ class ConfirmationOrderTypeTwoScreen extends StatelessWidget {
             ),
             Align(
               alignment: Alignment.bottomCenter,
-              child: Container(
-                width: double.maxFinite,
-                height: maxHeight * 0.6,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: AssetImage(
-                        "assets/images/survey_suggestion_background.png"),
-                  ),
-                  color: Color(0xFF99BFD4),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(40),
-                    topRight: Radius.circular(40),
-                  ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 22).copyWith(
-                    top: 0.03 * maxHeight,
-                  ),
-                  child: Column(children: [
-                    Padding(
-                      padding: EdgeInsets.only(right: maxWidth * 0.078),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          AutoSizeText(
-                            "Спасибо за ваши ответы!".toUpperCase(),
-                            style: const TextStyle(
-                                fontFamily: 'DrukCyr',
-                                fontSize: 50,
-                                color: Colors.white,
-                                height: 1),
-                            maxLines: 2,
-                          ),
-                          SizedBox(height: maxHeight * 0.0237),
-                          Padding(
-                            padding: EdgeInsets.only(right: maxWidth * 0.06),
-                            child: const AutoSizeText(
-                              'Мы рассмотрим ваш запрос и предоставим ответ как можно скорее',
-                              style: TextStyle(
-                                fontFamily: 'SourceSansPro',
-                                fontSize: 15,
-                                color: Colors.white,
-                              ),
-                              maxLines: 2,
-                            ),
-                          ),
-                        ],
+              child: Stack(
+                children: [
+                  Container(
+                    width: double.maxFinite,
+                    height: bodyHeight,
+                    decoration: const BoxDecoration(
+                      color: Color(0xFF99BFD4),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(40),
+                        topRight: Radius.circular(40),
                       ),
                     ),
-                    const Spacer(),
-                    Align(
-                      alignment: Alignment.bottomCenter,
-                      child: SizedBox(
-                        width: maxWidth,
-                        height: 0.055 * maxHeight,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            context.goNamed(AppRoutes.main.name);
-                          },
-                          style: ElevatedButton.styleFrom(
-                            elevation: 0,
-                            shape: const StadiumBorder(),
-                            foregroundColor:
-                                const Color.fromARGB(255, 183, 222, 243),
-                            backgroundColor: Colors.white,
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
+                  ),
+                  Positioned(
+                    right: 0,
+                    bottom: 0,
+                    child: Image.asset(
+                      'assets/images/sun_bottom.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Positioned(
+                    right: maxWidth * 0.2,
+                    bottom: 0,
+                    child: Image.asset(
+                      'assets/images/sign_bottom.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Container(
+                    width: double.maxFinite,
+                    height: bodyHeight,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(40),
+                        topRight: Radius.circular(40),
+                      ),
+                    ),
+                    child: Padding(
+                      padding:
+                          const EdgeInsets.symmetric(horizontal: 22).copyWith(
+                        top: 0.03 * maxHeight,
+                      ),
+                      child: Column(children: [
+                        Padding(
+                          padding: EdgeInsets.only(
+                              right: maxWidth * 0.078, top: maxHeight * 0.0237),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              AutoSizeText('НА ГЛАВНЫЙ ЭКРАН ',
+                              AutoSizeText(
+                                "Спасибо за ваши ответы!".toUpperCase(),
+                                style: const TextStyle(
+                                    fontFamily: 'DrukCyr',
+                                    fontSize: 50,
+                                    color: Colors.white,
+                                    height: 1),
+                                maxLines: 2,
+                              ),
+                              SizedBox(height: maxHeight * 0.0237),
+                              Padding(
+                                padding:
+                                    EdgeInsets.only(right: maxWidth * 0.06),
+                                child: const AutoSizeText(
+                                  'Мы рассмотрим ваш запрос и предоставим ответ как можно скорее',
                                   style: TextStyle(
                                     fontFamily: 'SourceSansPro',
-                                    fontSize: maxWidth > 320 ? 15 : 12,
-                                    color: const Color(0xFF86A6B8),
-                                  )),
-                              SvgPicture.asset(
-                                colorFilter: const ColorFilter.mode(
-                                    AppThemeColor.blueColor, BlendMode.srcIn),
-                                'assets/images/arrow_right.svg',
+                                    fontSize: 15,
+                                    color: Colors.white,
+                                  ),
+                                  maxLines: 2,
+                                ),
                               ),
                             ],
                           ),
                         ),
-                      ),
+                        const Spacer(),
+                        Align(
+                          alignment: Alignment.bottomCenter,
+                          child: SizedBox(
+                            width: maxWidth,
+                            height: 0.055 * maxHeight,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                context.goNamed(AppRoutes.main.name);
+                              },
+                              style: ElevatedButton.styleFrom(
+                                elevation: 0,
+                                shape: const StadiumBorder(),
+                                foregroundColor:
+                                    const Color.fromARGB(255, 183, 222, 243),
+                                backgroundColor: Colors.white,
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  AutoSizeText('НА ГЛАВНЫЙ ЭКРАН ',
+                                      style: TextStyle(
+                                        fontFamily: 'SourceSansPro',
+                                        fontSize: maxWidth > 320 ? 15 : 12,
+                                        color: const Color(0xFF86A6B8),
+                                      )),
+                                  SvgPicture.asset(
+                                    colorFilter: const ColorFilter.mode(
+                                        AppThemeColor.blueColor,
+                                        BlendMode.srcIn),
+                                    'assets/images/arrow_right.svg',
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        const BottomPadding(),
+                      ]),
                     ),
-                    const BottomPadding(),
-                  ]),
-                ),
+                  ),
+                ],
               ),
             )
           ],
