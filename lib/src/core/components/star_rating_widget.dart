69f6b97597e18a6490a0ff7a863d61fa04bdce64
Artem Luzin m <aratar006@gmail.com>
2024-04-18 18:24:27 +0700

    Добавить StarRatingWidget с возможностью изменения оценки

--- lib/src/core/components/star_rating_widget.dart
+++ lib/src/core/components/star_rating_widget.dart
@@ -22,3 +22,60 @@ class StarRatingWidget extends StatelessWidget {
     );
   }
 }
+
+class StarRatingWithGestureWidget extends StatefulWidget {
+  const StarRatingWithGestureWidget({
+    super.key,
+    this.size = 20,
+    this.rating = 0,
+  });
+  final double size;
+  final int rating;
+
+  @override
+  State<StarRatingWithGestureWidget> createState() =>
+      _StarRatingWithGestureWidgetState();
+}
+
+class _StarRatingWithGestureWidgetState
+    extends State<StarRatingWithGestureWidget> {
+  int _rating = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _rating = widget.rating;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        final pointerLocalPosition = context
+                .findRenderObject()!
+                .getTransformTo(null)
+                .getTranslation()
+                .x /
+            widget.size;
+        final tappedRating = (pointerLocalPosition / 1.25).round();
+
+        setState(() {
+          _rating = tappedRating;
+        });
+      },
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          for (int i = 0; i < 5; i++) ...[
+            Icon(
+              i < _rating ? Icons.star_rounded : Icons.star_outline_rounded,
+              color: i < _rating ? AppThemeColor.blueColor : AppThemeColor.gris,
+              size: widget.size,
+            ),
+            SizedBox(width: widget.size / 10),
+          ],
+        ],
+      ),
+    );
+  }
+}
