4437092ca659838223bbef45bda3ec9a5f5f624c
Artem Luzin m <aratar006@gmail.com>
2024-04-18 18:28:44 +0700

    Удалить лишний импорт

--- lib/src/feature/survey_order/ui/widgets/question_widget.dart
+++ lib/src/feature/survey_order/ui/widgets/question_widget.dart
@@ -1,7 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
-import 'package:my_app/src/core/components/drop_down_demo.dart';
-import 'package:my_app/src/core/components/dropdown_custom_widget.dart';
+import 'package:my_app/src/core/components/dropdown_custom_widget_new.dart';
 import 'package:my_app/src/core/theme/theme.dart';
 
 class QuestionWidget extends StatelessWidget {
