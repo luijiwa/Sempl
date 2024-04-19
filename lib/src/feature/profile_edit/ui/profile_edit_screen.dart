abf947d91723ffa1986086f3bef482ca016fabc6
Artem Luzin m <aratar006@gmail.com>
2024-04-19 20:00:49 +0700

    Сделать отсуп только для основных полей экрана

--- lib/src/feature/profile_edit/ui/profile_edit_screen.dart
+++ lib/src/feature/profile_edit/ui/profile_edit_screen.dart
@@ -7,18 +7,22 @@ import 'package:my_app/src/core/theme/theme.dart';
 import 'package:my_app/src/feature/survey/ui/widgets/address_data_fields_widget.dart';
 import 'package:my_app/src/feature/survey/ui/widgets/personal_data_fields_widget.dart';
 
-class ProfileEditScreen extends StatelessWidget {
+class ProfileEditScreen extends StatefulWidget {
   const ProfileEditScreen({super.key});
 
   @override
+  State<ProfileEditScreen> createState() => _ProfileEditScreenState();
+}
+
+class _ProfileEditScreenState extends State<ProfileEditScreen> {
+  @override
   Widget build(BuildContext context) {
     final double width = MediaQuery.sizeOf(context).width;
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: SingleChildScrollView(
-        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 22),
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        body: SingleChildScrollView(
+          keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -27,32 +31,48 @@ class ProfileEditScreen extends StatelessWidget {
                 title: const Text('SEMPL!'),
               ),
               SizedBox(height: width * 0.058535),
-              const AutoSizeText('Изменения будут сохранены в профиле '),
-              SizedBox(height: width * 0.04581),
-              const PersonalDataFieldsWidget(),
-              SizedBox(height: width * 0.13743),
-              const AddressDataFieldsWidget(),
-              SizedBox(height: width * 0.170515),
-              const Center(
-                child: AutoSizeText(
-                  '*Не найдено такой улицы',
-                  style: TextStyle(color: AppThemeColor.rose, fontSize: 12),
-                  maxLines: 1,
-                  minFontSize: 10,
-                  textAlign: TextAlign.center,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 22),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const AutoSizeText('Изменения будут сохранены в профиле '),
+                    SizedBox(height: width * 0.04581),
+                    const PersonalDataFieldsWidget(),
+                    SizedBox(height: width * 0.13743),
+                    const AddressDataFieldsWidget(),
+                    SizedBox(height: width * 0.170515),
+                    const Center(
+                      child: AutoSizeText(
+                        '*Не найдено такой улицы',
+                        style:
+                            TextStyle(color: AppThemeColor.rose, fontSize: 12),
+                        maxLines: 1,
+                        minFontSize: 10,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    const Center(
+                      child: AutoSizeText(
+                        '*Поля обязательные для заполнения',
+                        style:
+                            TextStyle(color: AppThemeColor.rose, fontSize: 12),
+                        maxLines: 1,
+                        minFontSize: 10,
+                      ),
+                    ),
+                    SizedBox(height: width * 0.02),
+                    NextStepButton(
+                        title: 'СОХРАНИТЬ ИЗМЕНЕНИЯ ',
+                        onPressed: () {
+                          // if (_addressFormKey.currentState!.validate()) {
+                          // } else {}
+                        }),
+                    const BottomPadding(),
+                  ],
                 ),
               ),
-              const Center(
-                child: AutoSizeText(
-                  '*Поля обязательные для заполнения',
-                  style: TextStyle(color: AppThemeColor.rose, fontSize: 12),
-                  maxLines: 1,
-                  minFontSize: 10,
-                ),
-              ),
-              SizedBox(height: width * 0.02),
-              NextStepButton(title: 'СОХРАНИТЬ ИЗМЕНЕНИЯ ', onPressed: () {}),
-              const BottomPadding(),
             ],
           ),
         ),
