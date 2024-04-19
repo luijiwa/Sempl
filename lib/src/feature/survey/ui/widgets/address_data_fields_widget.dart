cb778b00b2cd5734615bf8da1937672076818abe
Artem Luzin m <aratar006@gmail.com>
2024-04-19 20:01:23 +0700

    Поменять на statefull

--- lib/src/feature/survey/ui/widgets/address_data_fields_widget.dart
+++ lib/src/feature/survey/ui/widgets/address_data_fields_widget.dart
@@ -4,55 +4,73 @@ import 'package:my_app/src/core/components/required_input_field.dart';
 import 'package:my_app/src/core/components/text_input_field.dart';
 import 'package:my_app/src/core/theme/theme.dart';
 
-class AddressDataFieldsWidget extends StatelessWidget {
+class AddressDataFieldsWidget extends StatefulWidget {
   const AddressDataFieldsWidget({
     super.key,
   });
+  @override
+  State<AddressDataFieldsWidget> createState() =>
+      _AddressDataFieldsWidgetState();
+}
 
+class _AddressDataFieldsWidgetState extends State<AddressDataFieldsWidget> {
   @override
   Widget build(BuildContext context) {
     final double height = MediaQuery.of(context).size.height;
     final double width = MediaQuery.of(context).size.width;
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        AutoSizeText(
-          'АДРЕС ДОСТАВКИ',
-          style: Theme.of(context).textTheme.appProfileTitle,
-          maxLines: 1,
-        ),
-        SizedBox(height: height * 0.016),
-        const RequiredInputField(
-          hintText: 'Город',
-        ),
-        const SizedBox(height: 4),
-        const RequiredInputField(
-          hintText: 'Улица',
-        ),
-        const SizedBox(height: 4),
-        const Row(
-          children: [
-            Expanded(
-              child: RequiredInputField(hintText: 'Номер дома'),
-            ),
-            SizedBox(width: 4),
-            Expanded(
-              child: RequiredInputField(hintText: 'Номер квартиры'),
-            ),
-          ],
-        ),
-        const SizedBox(height: 4),
-        const Row(
-          children: [
-            Expanded(child: TextInputField(hintText: 'Подъезд')),
-            SizedBox(width: 4),
-            Expanded(
-              child: RequiredInputField(hintText: 'Почтовый индекс'),
-            ),
-          ],
-        ),
-      ],
+    return Form(
+      key: widget.key, // Привязка формы к GlobalKey
+
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          AutoSizeText(
+            'АДРЕС ДОСТАВКИ',
+            style: Theme.of(context).textTheme.appProfileTitle,
+            maxLines: 1,
+          ),
+          SizedBox(height: height * 0.016),
+          const RequiredInputField(
+            hintText: 'Город',
+          ),
+          const SizedBox(height: 4),
+          const RequiredInputField(
+            hintText: 'Улица',
+          ),
+          const SizedBox(height: 4),
+          const Row(
+            children: [
+              Expanded(
+                child: RequiredInputField(hintText: 'Номер дома'),
+              ),
+              SizedBox(width: 4),
+              Expanded(
+                child: RequiredInputField(hintText: 'Номер квартиры'),
+              ),
+            ],
+          ),
+          const SizedBox(height: 4),
+          Row(
+            children: [
+              const Expanded(child: TextInputField(hintText: 'Подъезд')),
+              const SizedBox(width: 4),
+              Expanded(
+                child: RequiredInputField(
+                  hintText: 'Почтовый индекс',
+                  isError: true,
+                  validator: (value) {
+                    // if (value.isEmpty) {
+                    //   return '';
+                    // }
+                    return null;
+                  },
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
