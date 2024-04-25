d198dee36ecb8e04f7571f7531a16b85eca0f96a
Artem Luzin m <aratar006@gmail.com>
2024-04-25 10:12:54 +0700

    Добавить анимацию нажатия

--- lib/src/feature/finished_sempls/ui/widgets/item_in_list_with_review_widget.dart
+++ lib/src/feature/finished_sempls/ui/widgets/item_in_list_with_review_widget.dart
@@ -14,95 +14,139 @@ class ItemInListWithReviewWidget extends StatefulWidget {
       _ItemInListWithReviewWidgetState();
 }
 
-class _ItemInListWithReviewWidgetState
-    extends State<ItemInListWithReviewWidget> {
+class _ItemInListWithReviewWidgetState extends State<ItemInListWithReviewWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animationTween;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      duration: Duration(milliseconds: 30),
+      vsync: this,
+    );
+    _animationTween =
+        Tween(begin: 0.0, end: 10.0).animate(_animationController);
+    _animationController.addListener(() {
+      setState(() {});
+    });
+  }
+
   final _showReview = ValueNotifier<bool>(false);
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
-    final offset = height * 0.029;
-    return InkWell(
-      onTap: () {
-        context.pushNamed(AppRoutes.itemScreen.name);
-      },
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: width * 0.05),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: offset),
-            if (widget.applyColorFilter)
-              ColorFiltered(
-                colorFilter: const ColorFilter.mode(
-                  AppThemeColor.grey,
-                  BlendMode.multiply,
-                ),
+    final offset = width * 0.06254661017;
+    return Material(
+      elevation: _animationTween.value,
+      borderRadius: BorderRadius.circular(20),
+      shadowColor: AppThemeColor.black.withOpacity(0.2),
+      child: GestureDetector(
+        onTapDown: _onTapDown,
+        onTapUp: _onTapUp,
+        onTap: () => context.pushNamed(AppRoutes.itemScreen.name),
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: width * 0.05),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: offset),
+              Padding(
+                padding: EdgeInsets.only(bottom: width * 0.01),
                 child: Image.asset("assets/images/item_image.png"),
-              )
-            else
-              Image.asset("assets/images/item_image.png"),
-            Text(
-              "Vinopure".toUpperCase(),
-              style: const TextStyle(fontSize: 15, fontWeight: FontWeight.w700),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: width * 0.05)
-                  .copyWith(top: 4, bottom: 11),
-              child: AutoSizeText(
-                  "Sérum salicylique Anti-imperfections 30ml - Caudalie"
-                      .toUpperCase(),
-                  textAlign: TextAlign.center,
-                  maxLines: 3,
-                  style: const TextStyle(
-                    fontSize: 13,
-                    height: 1,
-                  )),
-            ),
-            Text(
-              "5.0 из 89 отзывов".toUpperCase(),
-              style: const TextStyle(
-                fontSize: 8,
               ),
-            ),
-            const SizedBox(height: 11),
-            StarRatingWidget(
-              size: width * 0.038175,
-            ),
-            OutlinedButton(
-              onPressed: () {
-                setState(() {
-                  _showReview.value = !_showReview.value;
-                });
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("see review".toUpperCase()),
-                  AnimatedRotation(
-                    duration: const Duration(milliseconds: 200),
-                    turns: _showReview.value ? 0.5 : 0,
-                    child: const Icon(Icons.keyboard_arrow_down),
+              Text(
+                "Vinopure".toUpperCase(),
+                style:
+                    const TextStyle(fontSize: 15, fontWeight: FontWeight.w700),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: width * 0.05)
+                    .copyWith(top: width * 0.001, bottom: 11),
+                child: AutoSizeText(
+                    "Sérum salicylique Anti-imperfections 30ml - Caudalie"
+                        .toUpperCase(),
+                    textAlign: TextAlign.center,
+                    maxLines: 3,
+                    style: const TextStyle(
+                      fontSize: 13,
+                      height: 1,
+                    )),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: width * 0.3),
+                child: const AutoSizeText(
+                  "29/08/2023",
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: AppThemeColor.grisTwo,
                   ),
-                ],
+                  maxLines: 1,
+                ),
               ),
-            ),
-            AnimatedSize(
-              curve: Curves.easeInOutCirc,
-              duration: const Duration(milliseconds: 200),
-              child: _showReview.value
-                  ? const ReviewExpandedWidget()
-                  : const SizedBox.shrink(),
-            ),
-            SizedBox(height: offset),
-          ],
+              Padding(
+                padding:
+                    EdgeInsets.only(top: width * 0.0255, bottom: width * 0.05),
+                child: StarRatingWidget(
+                  size: width * 0.038175,
+                ),
+              ),
+              OutlinedButton(
+                onPressed: () {
+                  setState(() {
+                    _showReview.value = !_showReview.value;
+                  });
+                },
+                style: OutlinedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(
+                  vertical: width * 0.04,
+                  horizontal: width * 0.05,
+                )),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("see review".toUpperCase(),
+                        style: Theme.of(context).textTheme.appBodyMedium),
+                    AnimatedRotation(
+                      duration: const Duration(milliseconds: 200),
+                      turns: _showReview.value ? 0.5 : 0,
+                      child: const Icon(
+                        Icons.keyboard_arrow_down,
+                        color: AppThemeColor.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              AnimatedSize(
+                alignment: Alignment.topCenter,
+                curve: Curves.easeInOutCirc,
+                duration: const Duration(milliseconds: 200),
+                child: _showReview.value
+                    ? const ReviewExpandedWidget()
+                    : const SizedBox.shrink(),
+              ),
+              SizedBox(height: offset),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void _onTapDown(TapDownDetails details) {
+    _animationController.forward();
+  }
+
+  void _onTapUp(TapUpDetails details) {
+    _animationController.reverse();
   }
 }
 
@@ -115,46 +159,53 @@ class ReviewExpandedWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        StarRatingWidget(
-          rating: 3,
-          size: width * 0.038175,
-        ),
-        const Text(
-          '''Запах сыворотки неприятный. Для меня это просто «ух». Есть намек на что-то алкогольное, а также смесь трав. Возможно, такое неприятное сочетание вызвано эфирными маслами, которые я уже указала в составе.
-
+    return Padding(
+      padding: EdgeInsets.only(
+          top: width * 0.05629, bottom: width * 0.05629, right: width * 0.03),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          StarRatingWidget(
+            rating: 3,
+            size: width * 0.038175,
+          ),
+          SizedBox(height: width * 0.02545),
+          Text(
+            '''Запах сыворотки неприятный. Для меня это просто «ух». Есть намек на что-то алкогольное, а также смесь трав. Возможно, такое неприятное сочетание вызвано эфирными маслами, которые я уже указала в составе.
+    
 При нанесении сыворотки стараюсь сильно не дышать 🤪 жаль, что запах еще какое-то время задерживается на коже.
-
+    
 Сыворотка имеет гелевую текстуру. Довольно густой, не особенно жидкий. Он легко распределяется по коже и без особых усилий.''',
-        ),
-        ListView.builder(
-          shrinkWrap: true,
-          padding: EdgeInsets.zero,
-          physics: const NeverScrollableScrollPhysics(),
-          itemCount: 2,
-          itemBuilder: (context, index) {
-            return const PlusMinusDescriptionWidget(
-              icon: Icons.add_rounded,
-              text: 'Эффекта сужения пор в течение дня не получите',
-            );
-          },
-        ),
-        ListView.builder(
-          shrinkWrap: true,
-          padding: EdgeInsets.zero,
-          physics: const NeverScrollableScrollPhysics(),
-          itemCount: 2,
-          itemBuilder: (context, index) {
-            return const PlusMinusDescriptionWidget(
-              icon: Icons.remove_outlined,
-              text:
-                  'После ночного нанесения и умывания эффект не так выражен, как мне кажется',
-            );
-          },
-        )
-      ],
+            style: Theme.of(context).textTheme.appBodyMedium,
+          ),
+          SizedBox(height: width * 0.068715),
+          ListView.builder(
+            shrinkWrap: true,
+            padding: EdgeInsets.zero,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: 2,
+            itemBuilder: (context, index) {
+              return const PlusMinusDescriptionWidget(
+                icon: Icons.add_rounded,
+                text: 'Эффекта сужения пор в течение дня не получите',
+              );
+            },
+          ),
+          ListView.builder(
+            shrinkWrap: true,
+            padding: EdgeInsets.zero,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: 2,
+            itemBuilder: (context, index) {
+              return const PlusMinusDescriptionWidget(
+                icon: Icons.remove_outlined,
+                text:
+                    'После ночного нанесения и умывания эффект не так выражен, как мне кажется',
+              );
+            },
+          )
+        ],
+      ),
     );
   }
 }
@@ -180,7 +231,12 @@ class PlusMinusDescriptionWidget extends StatelessWidget {
           size: width * 0.04,
         ),
         const SizedBox(width: 10),
-        Flexible(child: Text(text)),
+        Flexible(
+          child: Text(
+            text,
+            style: Theme.of(context).textTheme.appBodyMedium,
+          ),
+        ),
         const SizedBox(width: 10),
       ],
     );
